7a3e2c4c7937c5e97e558b6db59b177d89dde349
Lijuce <441214473@qq.com>
2019-08-15 15:20:37 +0800

    run with command for convenience

--- word2vec_model.py
+++ word2vec_model.py
@@ -1,10 +1,23 @@
 import multiprocessing
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
+import os, sys, logging
 
-data_file = "wiki_data_seg.txt"
-print(LineSentence(data_file))
+logging.basicConfig(level=logging.INFO)
 
-model = Word2Vec(LineSentence(data_file), size=400, window=5, min_count=5, workers=multiprocessing.cpu_count())
-model.save("wiki.zh.text.model")
-# model.wv.save_word2vec("wiki.zh.text.vector", binary=False)
+if __name__ == '__main__':
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
+    logger.setLevel(level=logging.INFO)
+    logger.info("runnin %s" % ' '.join(sys.argv))
+    if len(sys.argv) < 4:
+        print ("No enough argv for python working")
+        sys.exit(1)
+    data_file, model_name, vector_pickle_name = sys.argv[1:4]  # 读取命令行的后三变量名
+
+    model = Word2Vec(LineSentence(data_file), size=400, window=5, min_count=5, workers=multiprocessing.cpu_count())
+    model.save(model_name)
+    model.wv.save_word2vec_format(vector_pickle_name, binary=False)
+    logger.info("Build model successfully!")
+
+
